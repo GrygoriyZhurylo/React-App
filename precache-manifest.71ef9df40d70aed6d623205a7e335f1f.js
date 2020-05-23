3cf11938bafaec2c2f803bc79b139337d195908c
Greg <grygoriy23@gmail.com>
2020-05-23 14:07:15 -0500

    Updates

---
+++ precache-manifest.71ef9df40d70aed6d623205a7e335f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9609250188ba6bfdfa372157ade84f34",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "597c07d521fdecc0e091",
+    "url": "/React-App/static/css/main.707c2d2e.chunk.css"
+  },
+  {
+    "revision": "2d0b30ba1c5a3b5ebf19",
+    "url": "/React-App/static/js/2.74ab614c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.74ab614c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597c07d521fdecc0e091",
+    "url": "/React-App/static/js/main.f0ef1da1.chunk.js"
+  },
+  {
+    "revision": "580a8311c0ad659f1dcc",
+    "url": "/React-App/static/js/runtime-main.baef1c9b.js"
+  }
+]);
